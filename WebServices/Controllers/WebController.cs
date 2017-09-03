03f575f5d8efcbf591e315d290b37de972f975e2
Aras <arkadiuszchr@gmail.com>
2017-09-03 20:39:58 +0200

    end of page

--- WebServices/Controllers/WebController.cs
+++ WebServices/Controllers/WebController.cs
@@ -1,17 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
+using WebServices.Models;
 
 namespace WebServices.Controllers
 {
-    public class WebController : Controller
+    public class WebController : ApiController
     {
-        // GET: Web
-        public ActionResult Index()
+        private readonly ReservationRepository _repo = ReservationRepository.Current;
+
+        public IEnumerable<Reservation> GetAllReservations()
         {
-            return View();
+            return _repo.GetAll();
+        }
+
+        public Reservation GetReservation(int id)
+        {
+            return _repo.Get(id);
+        }
+
+        [HttpPost]
+        public Reservation CreateReservation(Reservation item)
+        {
+            return _repo.Add(item);
+        }
+
+        [HttpPut]
+        public bool UpdateReservation(Reservation item)
+        {
+            return _repo.Update(item);
+        }
+
+        public void DeleteReservation(int id)
+        {
+            _repo.Remove(id);
         }
     }
 }
